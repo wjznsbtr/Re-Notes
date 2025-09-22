43239856827a378fdc875ece230aa2bfb6bccde8
wjzns <wjzns@outlook.com>
2025-09-22 22:12:21 +0800

    2025/09/22 22:12:21

--- 新建文本文档.bat
+++ 新建文本文档.bat
@@ -2,16 +2,14 @@
 echo Starting Git Bash from "Notes"
 start " "  "C:\Program Files\Git\git-bash.exe"
 
-set REPO_PATH = C:\Users\wjzns\Desktop\Notes
-cd %REPO_PATH%
-
 echo Checking for updates...
 git status
 
 echo Committing changes...
 git add .
-set /p cmt = 请输入commit：
-git commit -m "%cmt%"
+set d=%date:~0,10%
+set t=%time:~0,8%
+git commit -m "%d% %t%"
 
 echo Pushing to remote respository
 git push origin main
